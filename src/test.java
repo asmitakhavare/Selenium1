9d2608b46f0651b32c8414d253635b0a1d92c6d4
asmitakhavare <46107502+asmitakhavare@users.noreply.github.com>
2018-12-24 19:47:33 +0530

    Update test.java

--- src/test.java
+++ src/test.java
@@ -6,7 +6,8 @@ public class test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hi Asmita");
+System.out.println("Hi Asmita, how are you");
+		
 	}
 
 }
